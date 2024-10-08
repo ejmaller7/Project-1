27846513a382eb19c951f41244957f093f146180
Ethan Maller <mallerethan4@gmail.com>
2024-10-07 20:04:09 -0500

    Trying to add tailwind to the project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: [
+        "./App/**/*.{html,js,css}",
+        "./Components/**/*.{html,js,css}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
